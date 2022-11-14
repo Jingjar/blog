1eab285d6b857525c6087b1f68dbc3904186c56f
Tseng <a0979085925@gmail.com>
2022-11-14 14:11:21 +0800

    Route 路徑參數

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/hello2', 'App\Http\Controllers\SiteController@hello');
+Route::get('user2/{id?}', function ($id = 2) {
+    return 'User' . $id;
+});
+Route::get('user/{id}', 'App\Http\Controllers\UserController@hello');
+Route::get('posts/{post}/comments/{comments}', function ($post, $comment) {
+    return "參數一{$post},參數二{$comment}";
 });
